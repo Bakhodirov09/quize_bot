51f58c8bb1c1e7ef17d1c7db488a96ab59ddb895
Bahodirov Nurulloh <bloghash01@gmail.com>
2023-12-26 16:25:34 +0500

    first_commit

--- main/database_set.py
+++ main/database_set.py
@@ -1,14 +1,12 @@
 import databases
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from data.config import *
+from data.config import DB_URL
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-database = databases.Database(DATABASE_URL)
+database = databases.Database(DB_URL)
 
 Base = declarative_base()
 metadata = sqlalchemy.MetaData()
 
-engine = sqlalchemy.create_engine(DATABASE_URL)
+engine = sqlalchemy.create_engine(DB_URL)
 metadata.create_all(engine)
